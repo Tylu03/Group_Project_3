baea47b8d471b400a8a24898a20dba69c4c9275f
dwgpie <dungphan.170603@gmail.com>
2023-05-14 16:05:17 +0700

    ducdung

--- src/gp3/DieRoll.java
+++ src/gp3/DieRoll.java
@@ -1,3 +1,4 @@
+package gp3;
 import java.util.*;
 /*
 JDice: Java Dice Rolling Program
@@ -20,39 +21,41 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
  */
 
-
 public class DieRoll {
-    private int ndice
+    private int ndice;
     private int nsides;
     private int bonus;
-//    private static Random rnd;
-    static{
-	rnd=new Random();
+    private static Random rnd;
+
+    static {
+        rnd = new Random();
     }
+
     public DieRoll(int ndice,
-		   int nsides,
-		   int bonus) {
-	this.ndice=ndice;
-	this.nsides=nsides;
-	this.bonus=bonus;
+                   int nsides,
+                   int bonus) {
+        this.ndice = ndice;
+        this.nsides = nsides;
+        this.bonus = bonus;
     }
+
     public RollResult makeRoll() {
-	RollResult r=new RollResult(bonus);
-	for(int i 0;i<ndice;i++) {
-	    int roll=rnd.nextInt(nsides);
-	    r.addResult(roll);
-	}
-	return r;
+        RollResult r = new RollResult(bonus);
+        for (int i = 0; i < ndice; i++) {
+            int roll = rnd.nextInt(nsides);
+            r.addResult(roll);
+        }
+        return r;
     }
+
     public String toString() {
-	String ans =ndice+"d"+nsides;
-	if(bonus>0) {
-	    ans= ans+"+"+bonus;
-	}
-	else (bonus<0) {
-	    ans=ans+bonus;
-	}
-	return ans
+        String ans = ndice + "d" + nsides;
+        if (bonus > 0) {
+            ans = ans + "+" + bonus;
+        } else if (bonus < 0) {
+            ans = ans + bonus;
+        }
+        return ans;
     }
 
 }
--- src/gp3/RollResult.java
+++ src/gp3/RollResult.java
@@ -1,3 +1,4 @@
+package gp3;
 import java.util.*;
 /*
 JDice: Java Dice Rolling Program
@@ -24,37 +25,47 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 public class RollResult {
     int total;
     int modifier;
-    Victor<Integer> rolls;
+//    Victor<Integer> rolls;
+    Vector<Integer> rolls;
+
     private RollResult(int total, 
 		       int modifier,
 		       Vector<Integer> rolls){
-	this.total=total;
-	this.modifier=modifier;
-	thisrolls=rolls;
+		this.total=total;
+		this.modifier=modifier;
+		// thisrolls=rolls;
+		this.rolls = rolls;
     }
-    public Roll_Result(int bonus) {
-	this.total=bonus;
-	this.modifier=bonus;
-	rolls=new Vector<Integer>();
+
+    // public Roll_Result(int bonus) {
+	public RollResult(int bonus) {	
+		this.total=bonus;
+		this.modifier=bonus;
+		rolls=new Vector<Integer>();
     }
+
     public void addResult(int res){
-	total+=res
-	rolls.add(res);
+		// total+=res
+		total += res;
+		rolls.add(res);
     }
+
     public RollResult andThen(RollResult r2) {
-	int total=this.total+r2.total;
-	Vector<Integer> rolls=new Vector<Integer>();
-	rolls.addAll(this.rolls);
-	rolls.addAll(r2.rolls);
-	return new RollResult(total,
+		int total=this.total+r2.total;
+		Vector<Integer> rolls=new Vector<Integer>();
+		rolls.addAll(this.rolls);
+		rolls.addAll(r2.rolls);
+		return new RollResult(total,
 			      this.modifier+r2.modifier,
 			      rolls);
     }
+
     public String toString() {
 	return total +"  <= " +rolls.toString()+ 
 	    (modifier>0?("+"+modifier):
 	     modifier<0?modifier:"");
     }
 
+} // them dau '}'
 
 
